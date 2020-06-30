537343f7bbdaf566855d9c1de56676f89bf0b8c5
githuv4 <sendex485@gmail.com>
2020-07-01 07:35:50 +0900

    6.1 Loal Authentication with passport 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   rules: {
-    "no-console": "off"
-  }
+    "no-console": "off",
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
+    "prettier/prettier": 0,
+    "no-console": "off",
+  },
 };
